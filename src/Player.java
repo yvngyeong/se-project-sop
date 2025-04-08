d3621b38fdc673c146818b52fe389baf9870a9b9
yvngyeong <ygcho21@gmail.com>
2025-04-08 22:28:17 +0900

    change Player field

--- src/Player.java
+++ src/Player.java
@@ -3,7 +3,6 @@ import java.util.List;
 public class Player {
     private int id;
     private List<Piece> pieces;
-    private List<Integer> yutResult;
 
     public Player(int id){
         this.id = id;
@@ -19,10 +18,6 @@ public class Player {
 
     public List<Piece> getPieces() {
         return pieces;
-    }
-
-    public List<Integer> getYutResult() {
-        return yutResult;
     }
 
     public boolean checkWin(){
