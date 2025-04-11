6731b149c20b0d5f0bb46616a34b9f4f7a180272
yoon3660 <128508578+yoon3660@users.noreply.github.com>
2025-04-11 18:50:59 +0900

    Test Directoy

--- src/main/Service.java
+++ src/main/Service.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-import test.TestYut;
+import main.TestYut;
 
 public class Service {
     public void startGame() {
---
+++ src/test/java/GameTest/ServiceTest.java
@@ -0,0 +1,9 @@
+package GameTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ServiceTest {
+
+    @org.junit.jupiter.api.Test
+    void startGame() {
+    }
+}
