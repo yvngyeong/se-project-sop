312d878800b7df24c2db4936f62893e4bd239b11
sunggyeong <surreal0122@naver.com>
2025-05-05 22:12:01 +0900

    게임서비스화면, 윷관련컨트롤러구현

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import com.example.demo.*;
 import controller.GameController;
+import controller.ServiceController;
 import view.GameView;
 import view.ServiceView;
 import com.formdev.flatlaf.FlatLightLaf;
@@ -14,29 +15,7 @@ public class Main {
             System.err.println("FlatLaf 적용 실패: " + e.getMessage());
         }
 
-        ServiceView serviceView = new ServiceView();
-
-        serviceView.addStartButtonListener(e -> {
-            int playerCount = serviceView.getPlayerCount();
-            int pieceCount = serviceView.getPieceCount();
-            Yut yut = serviceView.getYutObject();
-            Board board = serviceView.getBoardObject();
-            board.createNodes();
-            board.createEdges();
-
-            Game game = new Game(playerCount, pieceCount, yut, board);
-            System.out.println("PlayerCount: " + playerCount);
-            System.out.println("PieceCount: " + pieceCount);
-            System.out.println("Yut: " + yut.getClass().getSimpleName());
-            System.out.println("Board: " + board.getClass().getSimpleName());
-
-            serviceView.dispose();
-
-            GameView gameView = new GameView(game);
-            gameView.setVisible(true);
-            GameController gameController = new GameController(game, gameView);
-//            gameController.run();
-        });
+        new ServiceController();
 
     }
 }
